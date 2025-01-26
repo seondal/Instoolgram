7f668f595aa3638c1dfc00e690edef06fb911b44
seondal <sseondal@gmail.com>
2025-01-27 03:53:54 +0900

    💄 style :  theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import "@/style/theme.css";
 
 import Script from "next/script";
 import { GoogleAnalytics } from "@next/third-parties/google";
-import { GOOGLE_ADSENSE, GOOGLE_ANALYITICS } from "@/constants/ENV";
+import { DEVELOPER, GOOGLE_ADSENSE, GOOGLE_ANALYITICS } from "@/constants/ENV";
 import { META_DATA } from "@/constants/META_DATA";
 
 export const metadata = META_DATA;
@@ -23,7 +23,16 @@ export default function RootLayout({
           crossOrigin="anonymous"></Script>
       </head>
       <body>
-        <header></header>
+        <header>
+          <nav>
+            <h1>
+              Ins
+              <strong className="text-pico-primary-background">tool</strong>
+              gram
+            </h1>
+            <a href={DEVELOPER}>made by Seondal</a>
+          </nav>
+        </header>
         <main className="">{children}</main>
         <GoogleAnalytics gaId={GOOGLE_ANALYITICS} />
         <footer className="text-center p-4"></footer>
