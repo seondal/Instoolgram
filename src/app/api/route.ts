f19f3661683db9a93ad50ea404e7bf3523d6859a
seondal <sseondal@gmail.com>
2025-01-27 05:27:52 +0900

    🌟 feat :  fetch

---
+++ src/app/api/route.ts
@@ -0,0 +1,25 @@
+import { NextRequest, NextResponse } from "next/server";
+
+const MAGIC_STRING = "?__a=1&__d=dis" as const;
+
+export async function GET(req: NextRequest) {
+  const url = req.nextUrl.searchParams.get("url");
+
+  if (!url) {
+    return NextResponse.error;
+  }
+
+  const new_url = url + MAGIC_STRING;
+
+  try {
+    const res = await fetch(new_url);
+    const data = await res.json();
+
+    const video_url = data.graphql.shortcode_media.video_url;
+
+    return NextResponse.json({ video: video_url });
+  } catch (error) {
+    console.log(error);
+    return NextResponse.error();
+  }
+}
